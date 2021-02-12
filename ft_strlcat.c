eea036af07e4a859b56147b59700ca32b46837d9
Leo Rapini <leorapini@gmail.com>
2021-02-12 18:42:55 -0300

    bug fix

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: lpinheir <lpinheir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/05 11:01:46 by lpinheir          #+#    #+#             */
-/*   Updated: 2021/02/12 00:59:25 by lpinheir         ###   ########.fr       */
+/*   Updated: 2021/02/12 18:33:43 by lpinheir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,26 +15,26 @@
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t	srclen;
-	size_t	total;
+	size_t	dstlen;
 	size_t	counter;
 
 	srclen = ft_strlen(src);
-	total = srclen + ft_strlen(dst);
+	dstlen = ft_strlen(dst);
 	counter = 0;
-	if (dstsize == 0)
-		return (srclen);
-	while (*dst != 0)
+	while (*dst != 0 && dstsize > 0)
 	{
 		dst++;
+		dstsize--;
 		counter++;
 	}
-	while (*src != 0 && counter < (total - 1))
+	while (*src != 0 && dstsize > 1)
 	{
 		*dst = *src;
 		src++;
 		dst++;
-		counter++;
+		dstsize--;
 	}
-	*dst = 0;
-	return (srclen);
+	if (dstsize > 0)
+		*dst++ = 0;
+	return (srclen + counter);
 }
