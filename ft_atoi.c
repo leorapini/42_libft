21b6b02d91ee5fc2134a85a702b08de6fead4259
Leo Rapini <leorapini@gmail.com>
2021-02-20 02:33:14 -0300

    bug fix

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: lpinheir <lpinheir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/08 12:58:38 by lpinheir          #+#    #+#             */
-/*   Updated: 2021/02/19 20:47:43 by lpinheir         ###   ########.fr       */
+/*   Updated: 2021/02/20 02:31:54 by lpinheir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,23 +27,24 @@ int	ft_atoi(const char *src)
 	int	sign;
 	int	control;
 
-	sign = 1;
+	sign = -1;
 	result = 0;
 	control = 0;
-	if (*src == 0)
-		return (0);
 	while (ft_isspace(*src))
 		src++;
 	if (*src == 45 || *src == 43)
-	{
-		if (*src == 45)
-			sign = sign * -1;
-		src++;
-	}
+		if (*src++ == 45)
+			sign = 1;
 	while (*src >= 48 && *src <= 57)
 	{
-		result = (result * 10) + (*src - '0');
-		src++;
+		result = (result * 10) - (*src++ - '0');
+		if (control < result)
+		{
+			if (sign < 0)
+				return (-1);
+			return (0);
+		}
+		control = result;
 	}
 	return (sign * result);
 }
